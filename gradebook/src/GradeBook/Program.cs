8ca9224414c3434f5b9173d8efc47dbb8efb11a0
Alexander Gluhov <alexander.gl@sbtech.com>
2019-06-20 16:51:41 +0300

    Add new project

---
+++ gradebook/src/GradeBook/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Concatenation example: dotnet run -- Alex (arugemnt)
+            // Console.WriteLine("Hello " + args[0] + "!");
+            if(args.Length > 0)
+            {
+                // interpolation example: dotnet run -- Alex (arugemnt)
+                Console.WriteLine($"Hello, {args[0]}!");
+            }
+            else
+            {
+                Console.WriteLine("Hello");
+            }
+        }
+    }
+}
