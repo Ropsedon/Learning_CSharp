1bc85dc8954782080abec3b7e9d1c1dc7719ca81
Alexander <glukhov.bank@gmail.com>
2019-10-20 23:34:45 +0300

    Introducing_C#

---
+++ Fundamentals/gradebook/src/GradeBook/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length > 1)
+            {
+                //Concatenation of string
+                Console.WriteLine("Hello " + args[0] + "!");
+                //Interpolation
+                Console.WriteLine($"Hello, {args[0]}!");
+            }
+            else 
+            {
+                Console.WriteLine("Hello!");
+            }
+        }
+    }
+}
