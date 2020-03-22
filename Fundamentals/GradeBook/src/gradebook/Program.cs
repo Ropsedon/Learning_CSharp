3ec534566e6cf62c5fdb5e3a600cbaa16fa4f052
Alexander <glukhov.bank@gmail.com>
2020-03-22 16:34:49 +0200

    Created list and array collection

--- Fundamentals/GradeBook/src/gradebook/Program.cs
+++ Fundamentals/GradeBook/src/gradebook/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace gradebook
+namespace GradeBook
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        
+            //Array collections
+            var arrayExample = new double[] { 11.5, 12.6, 14.7, 18.9 };
+
+            foreach (var item in arrayExample)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            //List collections
+            var listExample = new List<double>() { 19.9, 20.2, 30.2, 40.2, 50.2 };
+
+            foreach (var item2 in listExample)
+            {
+                System.Console.WriteLine(item2);
+            }
         }
     }
 }
