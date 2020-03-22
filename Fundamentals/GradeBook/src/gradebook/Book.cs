63b304e13c3e8b0b30116adaca8e99636b4b7a0e
Alexander <glukhov.bank@gmail.com>
2020-03-22 23:19:24 +0200

    Add statistic

--- Fundamentals/GradeBook/src/gradebook/Book.cs
+++ Fundamentals/GradeBook/src/gradebook/Book.cs
@@ -1,7 +1,60 @@
+using System;
+using System.Collections.Generic;
+
 namespace GradeBook
 {
     public class Book
-    {
-        
+    {  
+        //Constructor of the class
+        public Book(string name)
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
+
+        //Add value of collection
+        public void AddGrades(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        //Remove value of collection
+        public void RemoveGrade(double grade)
+        {
+            if(grades.Contains(grade))
+            {
+                grades.Remove(grade);
+            }
+            else
+            {
+                Console.WriteLine("Wrong value of grade!");
+            }
+        }
+
+        //Statistic of the collection
+        public void Statistic()
+        {
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+            var everageGrade = 0.0;
+
+            foreach(var number in grades)
+            {
+                lowGrade = Math.Min(number, lowGrade);
+                highGrade = Math.Max(number, highGrade);
+                everageGrade += number;
+            }
+
+            everageGrade /= grades.Count;
+
+            Console.WriteLine($"The lowest grade is: {lowGrade}");
+            Console.WriteLine($"The average grade is: {everageGrade:N2}");
+            Console.WriteLine($"The highest grade is: {highGrade}");
+        }
+
+     
+    private List<double> grades;
+    private string name;
+
     }
 }
--- Fundamentals/GradeBook/src/gradebook/Program.cs
+++ Fundamentals/GradeBook/src/gradebook/Program.cs
@@ -7,23 +7,11 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        
-            //Array collections
-            var arrayExample = new double[] { 11.5, 12.6, 14.7, 18.9 };
-
-            foreach (var item in arrayExample)
-            {
-                System.Console.WriteLine(item);
-            }
-
-            //List collections
-            var listExample = new List<double>() { 19.9, 20.2, 30.2, 40.2, 50.2 };
-
-            foreach (var item2 in listExample)
-            {
-                System.Console.WriteLine(item2);
-            }
+            var book = new Book("Alex's Grades");
+            book.AddGrades(89.99);
+            book.AddGrades(92.12);
+            book.AddGrades(99.99);
+            book.Statistic();
         }
     }
 }
